780b0050ab1074c0c609456e3ebe6809d5f46ca9
Carlos Daniel Estupiñan Diaz <128753592+DanielEstupinan@users.noreply.github.com>
2023-10-15 18:31:56 -0500

    Add files via upload

---
+++ review-1.js
@@ -0,0 +1,6 @@
+const tareas = [
+  { id: 1, descripcion: 'Hacer la compra', completada: false, descripcionAdicional: '¿Qué es mi producto y para que sirve?' },
+  { id: 2, descripcion: 'Lavar el coche', completada: true, descripcionAdicional: 'mi prodcuto es jabon quita manchas para cualquier clase de vehiculos, espejos, losa de cocina, sirve para quietar el 99.99% de bacterias' },
+  { id: 3, descripcion: 'Estudiar para el examen', completada: false, descripcionAdicional: '¿Cuáles son las funcionalidades más importantes y porque los usuarios las usarían?' }
+  { id: 4, descripcion: 'Estudiar para el examen', completada: false, descripcionAdicional: 'hace una limpieza profunda ya que elimina bacterias y eso protege tu salud, te funcionaria dado las enfermedades que existen por no lavasr cualquier cosa' }
+];
